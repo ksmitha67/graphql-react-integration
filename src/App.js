ca946098f6aecbe59c1bebea29d6ba1ae5dfce50
Kumari Smitha <ksmitha@deloitte.com>
2022-12-09 19:46:59 +0530

    graphql integration into react using class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import { GraphQLEditor } from 'graphql-editor';
+import FileSaver from 'file-saver';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import { render } from "react-dom";
+
+const types = {
+  name: `
+      type Query{
+          names: [Smita]
+      }
+  `,
+  nameLibrary: `
+      type Smita{
+          name: String;
+      }
+  `,
+};
+
+class App extends Component {
+  constructor() {
+    super()
+    
+    this.state = {
+      schema : {
+        code: types.name,
+        libraries: types.nameLibrary
+      }
+    }
+    this.myRef = React.createRef();
+  }
+
+  resetEditor(){
+    this.setState({
+      schema : {
+        code: '',
+        libraries: ''
+      }
+    })
+   
 }
+
+loadExisting(){
+    this.myRef.current.click();
+}
+
+commitGraph(){
+    var file = new File([this.state.schema.code], "hello world.graphqls", {type: "text/plain;charset=utf-8"});
+    FileSaver.saveAs(file);
+
+}
+
+handleData(e){
+    const data = e.target.result;
+    console.log("file data", data);
+    this.setState({
+      schema : {
+        code: data,
+        libraries: ''
+      }
+    })
+}
+
+handleChange(file){
+    let fileContent = new FileReader();
+    fileContent.onloadend  = (e)=>this.handleData(e);
+    console.log(fileContent);
+    fileContent.readAsText(file);
+
+}
+
+
+  render() {
+    return (
+      <div className='main'>
+            <div className='buttons'>
+                <button onClick={()=>this.resetEditor()}>Create New Graph</button>
+                <button onClick={()=>this.loadExisting()}>Load Existing</button>
+                <button onClick={()=>this.commitGraph()}>Commit graph</button>
+                <input type="file" 
+                    style={{display:"none"}}
+                    ref = {this.myRef}v
+                    onChange = {
+                        (e)=> this.handleChange(e.target.files[0])
+                    } 
+                />
+            </div>
+            <div className='editor'>
+        <GraphQLEditor
+            onSchemaChange={(props) => 
+                {
+                this.setState({
+                   props
+                })
+                // console.log(props)
+              }
+            }
+            schema={this.state.schema}
+        />
+        </div>
+        </div>
+    )
+  }
+}
+
 
 export default App;
